18bb23baefc44dc3a02f88a705442e71fcb5bcdd
Kazuo256 <kazuo256@gmail.com>
2016-01-27 10:16:21 -0200

    First commit

---
+++ activities/BootstrapActivity.lua
@@ -0,0 +1,16 @@
+
+local BootstrapActivity = require 'lux.class' :new{}
+
+BootstrapActivity:inherit(require 'Activity')
+
+function BootstrapActivity:instance (obj, ...)
+  
+  self:super(obj)
+
+  function obj.__accept:Load (engine)
+    -- Start here
+  end
+
+end
+
+return BootstrapActivity
