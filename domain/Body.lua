896717135003ae6212e7eb6671dbd7a25c141b24
Kazuo256 <kazuo256@gmail.com>
2016-01-27 18:27:51 -0200

    Body drawing order
    Draw bodies by kind

--- domain/Body.lua
+++ domain/Body.lua
@@ -1,9 +1,17 @@
 
 local Body = class:new{}
 
+local groups = {}
+
 function Body:instance (obj, kind)
   
   local position = vec2:new{}
+
+  if not groups[kind] then
+    groups[kind] = {}
+  end
+
+  groups[kind][obj] = true
 
   function obj:getPosition ()
     return position:clone()
@@ -21,6 +29,18 @@ function Body:instance (obj, kind)
     position:add(diff)
   end
 
+end
+
+function Body:forKind (kind)
+  if groups[kind] then
+    return pairs(groups[kind])
+  else
+    return pairs {}
+  end
+end
+
+function Body:onDestroy (body)
+  groups[body:getKind()][body] = nil
 end
 
 return require 'Domain' (Body)
--- resources/drawstep/display.lua
+++ resources/drawstep/display.lua
@@ -5,9 +5,9 @@ local function triple (x)
   return x, x ,x
 end
 
-local kind = {}
+local drawkind = {}
 
-function kind.player (graphics, engine, id)
+function drawkind.player (graphics, engine, id)
   local grow = 1 + (engine:server'ODA':pulse(4) > 0.8 and 0.2 or 0)
   graphics.scale(grow, grow)
   graphics.setColor(triple(grow*128))
@@ -18,11 +18,11 @@ function kind.player (graphics, engine, id)
   graphics.polygon('fill', 0, 0, .3, -.2, -.3, -.2)
 end
 
-function kind.weak_shot (graphics, engine, id)
+function drawkind.weak_shot (graphics, engine, id)
   graphics.circle('fill', 0, 0, .1, 8)
 end
 
-function kind.evil_square (graphics, engine, id)
+function drawkind.evil_square (graphics, engine, id)
   graphics.setColor(200, 100, 100)
   graphics.polygon('fill', 0, 0, .5, .5, -.5, .5)
   graphics.setColor(250, 150, 150)
@@ -35,6 +35,10 @@ end
 
 local display = {}
 
+local draw_order = {
+  'player', 'evil_square', 'weak_shot'
+}
+
 function display.update (dt)
   -- nothing for now
 end
@@ -44,12 +48,15 @@ function display.draw (graphics, engine)
   graphics.push()
   graphics.translate(W/2, 7*H/8)
   graphics.scale(32, -32)
-  for id,body in Body:all() do
-    graphics.push()
-    graphics.translate(body:getPosition():unpack())
-    kind[body:getKind()] (graphics, engine, id)
-    graphics.setColor(triple(255))
-    graphics.pop()
+  for _,kind in ipairs(draw_order) do
+    for body in Body:forKind(kind) do
+      local id = Body:getId(body)
+      graphics.push()
+      graphics.translate(body:getPosition():unpack())
+      drawkind[body:getKind()] (graphics, engine, id)
+      graphics.setColor(triple(255))
+      graphics.pop()
+    end
   end
   graphics.pop()
 end
